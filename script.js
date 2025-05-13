5cd467033313d199effeda2ed8d7fbda32892403
yurizindo1a <137806335+yurizindo1a@users.noreply.github.com>
2025-05-13 07:53:56 -0300

    eee

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
